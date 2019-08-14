94e4f7144327b630399c01b5ea36b5167afc37b0
Vyacheslav Glushko <mail@ixax.me>
2019-08-14 12:53:00 +0300

    headings

--- src/js/index.js
+++ src/js/index.js
@@ -1,31 +1,26 @@
-export default (() => {
-
-    document.addEventListener('DOMContentLoaded', () => {
-        console.log('asd');
-        const inputArr = Array.from(document.getElementsByClassName('input-resize-js'));
-        const hide = document.getElementById('hide');
-        const hideSmall = document.getElementById('hide-small');
-
-        function checkInputWidth(target, self = true) {
-            const input = self ? this : target;
-            const hideInput = input.clientHeight > 40 ? hide : hideSmall;
-
-            hideInput.textContent = input.value;
-            input.style.width = hideInput.offsetWidth + "px";
-        }
-
-        inputArr.forEach(input => {
-            checkInputWidth(input, false);
-
-            input.addEventListener('input', checkInputWidth);
-        });
-
-        const observerCollection = document.getElementsByClassName('observer');
-        Array.from(observerCollection).forEach(item => {
-            const observer = new MutationObserver(() => inputArr.forEach(input => checkInputWidth(input, false)));
-
-            observer.observe(item, {attributes: true});
-        });
-    });
-
-});
+// document.addEventListener('DOMContentLoaded', () => {
+//     const inputArr = Array.from(document.getElementsByClassName('js-resize-input'));
+//     const hide = document.getElementById('hide');
+//     const hideSmall = document.getElementById('hide-small');
+//
+//     function checkInputWidth(target, self = true) {
+//         const input = self ? this : target;
+//         const hideInput = input.clientHeight > 40 ? hide : hideSmall;
+//
+//         hideInput.textContent = input.value;
+//         input.style.width = hideInput.offsetWidth + "px";
+//     }
+//
+//     inputArr.forEach(input => {
+//         checkInputWidth(input, false);
+//
+//         input.addEventListener('input', checkInputWidth);
+//     });
+//
+//     const observerCollection = document.getElementsByClassName('observer');
+//     Array.from(observerCollection).forEach(item => {
+//         const observer = new MutationObserver(() => inputArr.forEach(input => checkInputWidth(input, false)));
+//
+//         observer.observe(item, {attributes: true});
+//     });
+// });
