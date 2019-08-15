fd8ba6f5c594c5f45b978231ea24bead6c80321d
Vyacheslav Glushko <mail@ixax.me>
2019-08-15 22:56:38 +0300

    remove unused postcss plugins

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,7 @@
 module.exports = {
     plugins: [
         require('autoprefixer'),
-        require('postcss-extend'),
-        require('postcss-hexrgba'),
         require('postcss-import'),
-        require('postcss-sassy-mixins'),
         require('postcss-nested'),
         require('cssnano'),
     ],
