57039c97bb763838847c0dbbef89af2a1218319f
Vyacheslav Glushko <mail@ixax.me>
2019-08-23 14:25:42 +0300

    move portfolio to json file

--- src/js/main.js
+++ src/js/main.js
@@ -19,27 +19,30 @@ $(document).ready(() => {
         }, 400);
     });
 
-    const $portfolioCarousel = $('#portfolioCarousel');
-    initPortfolio(
-        $portfolioCarousel.find('.js-items'),
-        window.WORKS.filter(item => item.showInPortfolio),
-    );
-    carousel($portfolioCarousel);
+    $.getJSON(`works.json?_=${Date.now()}`, (loadedWorks) => {
+        const $portfolioCarousel = $('#portfolioCarousel');
+        initPortfolio(
+            $portfolioCarousel.find('.js-items'),
+            loadedWorks,
+        );
+        carousel($portfolioCarousel);
 
-    const $feedbackCarousel = $('#feedbackCarousel');
-    initFeedback(
-        $feedbackCarousel.find('.js-items'),
-        window.WORKS.filter(item => item.feedback),
-    );
-    carousel($feedbackCarousel);
+        const $feedbackCarousel = $('#feedbackCarousel');
+        initFeedback(
+            $feedbackCarousel.find('.js-items'),
+            loadedWorks.filter(item => item.feedback),
+        );
+        carousel($feedbackCarousel);
 
-    $(document).on('click', '.js-open-work', (evt) => {
-        const $link = $(evt.currentTarget);
-        const workId = $link.data('workId');
-        const work = window.WORKS.find(work => work.id === workId);
+        $(document).on('click', '.js-open-work', (evt) => {
+            const $link = $(evt.currentTarget);
+            const workId = $link.data('workId');
+            const work = loadedWorks.find(work => work.id === workId);
 
-        if (work) {
-            openWorkModal(work);
-        }
+            if (work) {
+                openWorkModal(work);
+            }
+        });
+
     });
 });
