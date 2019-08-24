6fdb3f4f3c5307ee9d6133dadbd9438b813a86d0
Vyacheslav Glushko <mail@ixax.me>
2019-08-24 21:00:06 +0300

    done with all forms

--- src/js/modalApplication.js
+++ src/js/modalApplication.js
@@ -1,12 +1,18 @@
-import loadTemplate from './template';
+import initForm from './form';
 import openModal from './modal';
+import loadTemplate from './template';
 
 export default function openApplicationModal(data) {
     const template = loadTemplate('application');
     const render = template(data);
 
-    return openModal({
+    const modal = openModal({
         name: 'application',
         render,
     });
+
+    initForm(modal.$modal.find('.js-form-wrapper'));
+    modal.$modal.find('input:first').focus();
+
+    return modal;
 }
