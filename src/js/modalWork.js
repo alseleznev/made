5a1ba04eafa503646f5989ec06ebcd8f3964b27b
Vyacheslav Glushko <mail@ixax.me>
2019-09-01 18:28:04 +0300

    remove unused modalWork code

--- src/js/modalWork.js
+++ src/js/modalWork.js
@@ -3,15 +3,12 @@ import openModal from './modal';
 import loadTemplate from './template';
 
 export default function openWorkModal(data) {
-    const maxWidth = Math.min($(window).width(), 690 - 20 * 2);
     const template = loadTemplate('work');
     const render = template(data);
     const modal = openModal({
         name: 'work',
         render,
     });
-
-    // modal.$modal.find('.modal-work').css('max-width', maxWidth);
 
     const $carousel = modal.$modal.find('.js-carousel');
     carousel($carousel, {
