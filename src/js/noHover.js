66a3fed35cbacac0f481741162ecabe5cf23eca7
Vyacheslav Glushko <mail@ixax.me>
2019-09-01 14:02:50 +0300

    hasTouch class to body

--- src/js/noHover.js
+++ src/js/noHover.js
@@ -1,28 +1,20 @@
-function hasTouch() {
-    return 'ontouchstart' in document.documentElement
-        || navigator.maxTouchPoints > 0
-        || navigator.msMaxTouchPoints > 0;
-}
-
 export default function noHover() {
-    if (hasTouch()) {
-        try {
-            for (let si in document.styleSheets) {
-                const styleSheet = document.styleSheets[si];
-                if (!styleSheet.rules) {
+    try {
+        for (let si in document.styleSheets) {
+            const styleSheet = document.styleSheets[si];
+            if (!styleSheet.rules) {
+                continue;
+            }
+
+            for (let ri = styleSheet.rules.length - 1; ri >= 0; ri--) {
+                if (!styleSheet.rules[ri].selectorText) {
                     continue;
                 }
 
-                for (let ri = styleSheet.rules.length - 1; ri >= 0; ri--) {
-                    if (!styleSheet.rules[ri].selectorText) {
-                        continue;
-                    }
-
-                    if (styleSheet.rules[ri].selectorText.match(':hover')) {
-                        styleSheet.deleteRule(ri);
-                    }
+                if (styleSheet.rules[ri].selectorText.match(':hover')) {
+                    styleSheet.deleteRule(ri);
                 }
             }
-        } catch (ex) {}
-    }
+        }
+    } catch (ex) {}
 }
