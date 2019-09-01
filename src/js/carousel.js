2a6a0e76c01b9ff88dcee1d98e4e29bef9ab987e
Vyacheslav Glushko <mail@ixax.me>
2019-09-01 19:07:57 +0300

    disable carousel drag for non-touch devices

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -1,11 +1,19 @@
 import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel';
 
+import hasTouch from './hasTouch';
+
 export default function carousel($carousel, options) {
+    const supportsTouch = hasTouch();
+
     return $carousel.owlCarousel({
         navText: '',
         loop: true,
         margin: 5,
+        mouseDrag: supportsTouch,
+        touchDrag: supportsTouch,
+        pullDrag: supportsTouch,
+        freeDrag: supportsTouch,
         ...options,
     });
 }
